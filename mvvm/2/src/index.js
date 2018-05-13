78f256aa3fcead8af6a79e659f5627c0b7ba42ce
xclazy <sue561833@qq.com>
2018-05-13 17:13:43 +0800

    mvvm 2.2

--- mvvm/2/src/index.js
+++ mvvm/2/src/index.js
@@ -1,12 +1,18 @@
 import san from 'san';
 import { router } from 'san-router';
 
-import test_2 from './page/test_2';
+import test2_1 from './page/2_1';
+import test2_2 from './page/2_2';
 
 
 router.add({
-  rule: '/2',
-  Component: test_2,
+  rule: '/2_1',
+  Component: test2_1,
+  target: '#app',
+})
+router.add({
+  rule: '/2_2',
+  Component: test2_2,
   target: '#app',
 })
 
